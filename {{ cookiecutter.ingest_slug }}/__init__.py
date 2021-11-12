9b6156ff64b7c92a4539d9b5049af2ccabf6c7e0
Maxwell Levin <maxwellevin@users.noreply.github.com>
2021-11-12 12:14:19 -0800

    Update imports

--- {{ cookiecutter.ingest_slug }}/__init__.py
+++ {{ cookiecutter.ingest_slug }}/__init__.py
@@ -1,7 +1,2 @@
-from .pipeline import (
-    Pipeline,
-    pipeline,
-    {% if cookiecutter.use_custom_filehandler == "yes" %}filehandler,{% endif %}
-    {% if cookiecutter.use_custom_qc == "yes" %}qc,{% endif %}
-)
+from .pipeline import *
 from .mapping import mapping
